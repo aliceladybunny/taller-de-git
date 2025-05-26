a612376861583b89457247b46122040f0ea76bff
Alice Gonzalez Fernandez <aliceladybunny@gmail.comgit config --global user.email aliceladybunny@gmail.com>
2025-05-26 15:22:36 +0200

    Programa interactivo

--- hola.php
+++ hola.php
@@ -1,5 +1,5 @@
 <?php
-// Autor: Alice González
-// El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+// Autor: Alice González <aliceladybunny@gmail.com>
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";
